65033cd8a5a81f31ffe54f227d456871302ba25d
Andrey Filippov <andrey@elphel.com>
2014-02-02 17:49:33 -0700

    Horizontal separator for the dialogs

---
+++ src/com/elphel/vdt/ui/options/component/LabelComponent.java
@@ -0,0 +1,50 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Elphel, Inc.
+ * This file is a part of Eclipse/VDT plug-in.
+ * Eclipse/VDT plug-in is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Eclipse/VDT plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package com.elphel.vdt.ui.options.component;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+import com.elphel.vdt.core.tools.params.Parameter;
+
+public class LabelComponent extends GeneralComponent {
+
+	private Label labelField;
+
+	public LabelComponent(Parameter param) {
+		super(param);
+	}
+
+	public void createControl(Composite parent) {
+		super.createControl(parent);
+		GridData gridData = new GridData(GridData.HORIZONTAL_ALIGN_FILL);
+		gridData.horizontalSpan = 2;
+		labelField= new Label(parent,SWT.HORIZONTAL | SWT.SEPARATOR);
+		labelField.setLayoutData(gridData);
+	}
+
+	public void setSelection(String paramValue) {}
+	protected String getSelection() {return null;}
+	protected boolean isDisposed() {return false;}
+	public void setFocus() { }
+	protected void addListeners() { }
+	protected void removeListeners() {}
+	protected void switchState(boolean defaulted) {}
+
+} // class LabelComponent
