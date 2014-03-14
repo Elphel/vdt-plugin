703c6d5cfcbea285186d0717926097e73c94f746
Andrey Filippov <andrey@elphel.com>
2014-03-14 16:47:49 -0600

    Added license header to TSL XML files, updated copyright date on VDT files

--- src/com/elphel/vdt/ui/preferences/ScrolledPageContent.java
+++ src/com/elphel/vdt/ui/preferences/ScrolledPageContent.java
@@ -1,19 +1,19 @@
 /*******************************************************************************
+ * Copyright (c) 2014 Elphel, Inc.
  * Copyright (c) 2006 Elphel, Inc and Excelsior, LLC.
  * This file is a part of Eclipse/VDT plug-in.
  * Eclipse/VDT plug-in is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your option)
- * any later version.
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Eclipse/VDT plug-in is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * Eclipse/VDT plug-in is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along
- * with Eclipse VDT plug-in; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *******************************************************************************/
 /*******************************************************************************
  * Copyright (c) 2000, 2005 IBM Corporation and others.
