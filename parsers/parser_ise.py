f9cb9126826bba7144ed1f3b0042256d09f112be
Andrey Filippov <andrey@elphel.com>
2014-04-09 16:45:42 -0600

    Added file headers

--- parsers/parser_ise.py
+++ parsers/parser_ise.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python
-##/usr/bin/python -u
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013, Elphel.inc.
+# configuration of the DDR-related registers
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+#  Additional permission under GNU GPL version 3 section 7:
+#  If you modify this Program, or any covered work, by linking or combining it
+#  with Eclipse or Eclipse plugins (or a modified version of those libraries),
+#  containing parts covered by the terms of EPL/CPL, the licensors of this
+#  Program grant you additional permission to convey the resulting work.
+#  {Corresponding Source for a non-source form of such a combination shall
+#  include the source code for the parts of Eclipse or Eclipse plugins used
+#  as well as that of the covered work.}
+ 
+__author__ = "Andrey Filippov"
+__copyright__ = "Copyright 2014, Elphel, Inc."
+__license__ = "GPL"
+__version__ = "3.0+"
+__maintainer__ = "Andrey Filippov"
+__email__ = "andrey@elphel.com"
+__status__ = "Development"
 import sys
 import re
 patternBit=re.compile("_\d+$")
