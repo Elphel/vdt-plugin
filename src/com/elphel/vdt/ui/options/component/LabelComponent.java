9bea7c8c572d1b461e709b6280cd498f95f17af2
Andrey Filippov <andrey@elphel.com>
2014-06-14 10:57:41 -0600

    Renamed Eclipse/VDT plug-in to just VDT plug-in

--- src/com/elphel/vdt/ui/options/component/LabelComponent.java
+++ src/com/elphel/vdt/ui/options/component/LabelComponent.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2014 Elphel, Inc.
- * This file is a part of Eclipse/VDT plug-in.
- * Eclipse/VDT plug-in is free software; you can redistribute it and/or modify
+ * This file is a part of VDT plug-in.
+ * VDT plug-in is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Eclipse/VDT plug-in is distributed in the hope that it will be useful,
+ * VDT plug-in is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
